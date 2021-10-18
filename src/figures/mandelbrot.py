b727eb35ec5e383042efdec68faafaa4f83c4ab8
Rodrigo Luger <rodluger@gmail.com>
2021-10-18 15:33:45 -0400

    example

--- src/figures/mandelbrot.py
+++ src/figures/mandelbrot.py
@@ -39,3 +39,20 @@ plt.imshow(
 )
 plt.gca().axis("off")
 fig.savefig("mandelbrot.pdf", bbox_inches="tight")
+
+
+# The same figure, but in red!
+fig = plt.figure(figsize=(8, 8))
+mandelbrot_set = np.round(1 - compute_mandelbrot(500, 50.0, 601, 401))
+tab10 = copy.copy(plt.get_cmap("Set1"))
+tab10.set_over("w")
+plt.imshow(
+    mandelbrot_set.T,
+    extent=[-2, 1, -1.5, 1.5],
+    interpolation="nearest",
+    cmap=tab10,
+    vmin=0,
+    vmax=0.9,
+)
+plt.gca().axis("off")
+fig.savefig("mandelbrot_red.pdf", bbox_inches="tight")
