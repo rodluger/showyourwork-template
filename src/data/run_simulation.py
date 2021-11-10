69b15787c663276addf04c2e8eff911d753aaaae
Rodrigo Luger <rodluger@gmail.com>
2021-11-09 20:11:40 -0500

    add sim

--- src/data/run_simulation.py
+++ src/data/run_simulation.py
@@ -3,15 +3,13 @@ Run a (fake) simulation and generate a simulation results file.
 
 """
 import numpy as np
-import sys
 
-# Get the command line argument
-value = int(sys.argv[1])
+for value in range(10):
 
-# Generate an array of random numbers with a given seed.
-# This is a placeholder for an "expensive" operation.
-np.random.seed(value)
-X = np.random.randn(10)
+    # Generate an array of random numbers with a given seed.
+    # This is a placeholder for an "expensive" operation.
+    np.random.seed(value)
+    X = np.random.randn(10)
 
-# Save the "simulation" results to disk
-np.savetxt(f"src/data/results_{value}.dat", X=X)
+    # Save the "simulation" results to disk
+    np.savetxt(f"results_{value}.dat", X=X)
